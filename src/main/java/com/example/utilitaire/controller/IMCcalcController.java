2d5bced1f2a88e50bd5ea79015181e8132db7fe0
David <david.roquain@edu.esiee-it.fr>
2021-12-08 11:21:39 +0100

    Add IMC calc
    oups

--- src/main/java/com/example/utilitaire/controller/IMCcalcController.java
+++ src/main/java/com/example/utilitaire/controller/IMCcalcController.java
@@ -2,13 +2,16 @@ package com.example.utilitaire.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ConvertDecBinController implements Initializable {
+public class IMCcalcController implements Initializable {
     @FXML
-
+    private TextField InputTaille;
+    @FXML
+    private TextField InputPoids;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- src/main/java/com/example/utilitaire/controller/MainController.java
+++ src/main/java/com/example/utilitaire/controller/MainController.java
@@ -2,16 +2,26 @@ package com.example.utilitaire.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-
+    private Menu menuIMCcalc;
+    @FXML
+    private AnchorPane main;
+    @FXML
+    private HBox formIMCcalc;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        main.getChildren().removeAll(formIMCcalc);
+        menuIMCcalc.setOnMenuValidation(launchbinary ->{
+            main.getChildren().removeAll(formIMCcalc);
+            main.getChildren().add(formIMCcalc);
+        });
     }
 }
