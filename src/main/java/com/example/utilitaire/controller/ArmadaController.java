46f4f29a1f0d4273f6451716624d1ee4ff34ae69
David <david.roquain@edu.esiee-it.fr>
2021-12-08 15:04:35 +0100

    Armée

---
+++ src/main/java/com/example/utilitaire/controller/ArmadaController.java
@@ -0,0 +1,29 @@
+package com.example.utilitaire.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ArmadaController implements Initializable {
+    @FXML
+    private TreeView treemada;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TreeItem<String> armada = new TreeItem<>("Armée");
+        TreeItem<String> general = new TreeItem<>("Général");
+        TreeItem<String> soldat = new TreeItem<>("Soldat");
+        general.getChildren().add(soldat);
+        armada.getChildren().add(general);
+        treemada.setRoot(armada);
+    }
+}
