c48f7b6291e14cad45ed7144d3f4ee2126e1dff9
angelrafael <generalpompeyo@hotmail.com>
2021-12-08 14:14:56 +0100

    - Finalisation de l'interface de la bibliothèque - Ajout afichage bibliothèque au MainController

--- src/main/java/com/example/utilitaire/controller/MainController.java
+++ src/main/java/com/example/utilitaire/controller/MainController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -16,18 +17,32 @@ public class MainController implements Initializable {
     @FXML
     private Menu menuIMCcalc;
     @FXML
+    private Menu menuLib;
+    @FXML
     private MenuItem Yes;
     @FXML
     private AnchorPane main;
     @FXML
     private HBox formIMCcalc;
+    @FXML
+    private VBox formLib;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        main.getChildren().removeAll(formIMCcalc);
+        main.getChildren().removeAll(formIMCcalc,formLib);
+
         menuIMCcalc.setOnMenuValidation(launchbinary ->{
-            main.getChildren().removeAll(formIMCcalc);
+            main.getChildren().removeAll(formIMCcalc,formLib);
             main.getChildren().add(formIMCcalc);
         });
+
+        menuLib.setOnMenuValidation(launchLibrary -> {
+            main.getChildren().removeAll(formLib,formIMCcalc);
+            main.getChildren().add(formLib);
+        });
+
+
+
         Yes.setOnAction(exit ->{
             Platform.exit();
         });
