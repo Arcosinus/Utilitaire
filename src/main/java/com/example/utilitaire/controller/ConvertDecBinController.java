b3840c1d5449e2a403de8cb9a501ae3c74353d6f
David <david.roquain@edu.esiee-it.fr>
2021-12-08 10:39:21 +0100

    Add ConvertDecBin scene

--- src/main/java/com/example/utilitaire/controller/ConvertDecBinController.java
+++ src/main/java/com/example/utilitaire/controller/ConvertDecBinController.java
@@ -2,12 +2,11 @@ package com.example.utilitaire.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class ConvertDecBinController implements Initializable {
     @FXML
 
     @Override
