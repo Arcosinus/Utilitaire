c8ea916107e07658365585a1b3a6ab4d4868a354
Leo <leojj.pro@gmail.com>
2021-12-08 15:42:53 +0100

    menu done and bhconverter implemented

--- src/main/java/com/example/utilitaire/controller/MainController.java
+++ src/main/java/com/example/utilitaire/controller/MainController.java
@@ -1,14 +1,85 @@
 package com.example.utilitaire.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Menu menuIMCcalc;
+    @FXML
+    private HBox formIMCcalc;
+
+    @FXML
+    private Menu menuBHConverter;
+    @FXML
+    private HBox formBHConverter;
+
+    @FXML
+    private Menu menuLib;
+    @FXML
+    private HBox formLib;
+
+    @FXML
+    private Menu menuArmee;
+    @FXML
+    private HBox formArmee;
+
+    @FXML
+    private Menu menuRomain;
+    @FXML
+    private HBox formRomain;
+
+
+    @FXML
+    private MenuItem Close;
+    @FXML
+    private VBox main;
+    @FXML
+    private AnchorPane mainAnchor;
+
+//ConversionRomain RomainControlleur formRomain,
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mainAnchor.getChildren().removeAll(formBHConverter/**, formIMCcalc, formLib, formArmee, formRomain**/);
+
+        menuIMCcalc.setOnMenuValidation(launchbinary ->{
+            main.getChildren().removeAll(formIMCcalc,formLib,formBHConverter/**,formRomain,formArmee**/);
+            main.getChildren().add(formIMCcalc);
+        });
+
+        menuLib.setOnMenuValidation(launchLibrary -> {
+            main.getChildren().removeAll(formIMCcalc,formLib,formBHConverter/**,formRomain,formArmee**/);
+            main.getChildren().add(formLib);
+        });
+
+        menuBHConverter.setOnMenuValidation(launchBHconverter ->{
+            main.getChildren().removeAll(formIMCcalc,formLib,formBHConverter/**,formRomain,formArmee**/);
+            main.getChildren().add(formBHConverter);
+        });
+
+        //menuArmee.setOnMenuValidation(launchArmee ->{
+        //    main.getChildren().removeAll(formIMCcalc,formLib,formBHConverter/**,formRomain,formArmee**/)
+        //    main.getChildren().add(formArmee);
+        //});
+
+        //menuRomain.setOnMenuValidation(launchRomain ->{
+        //    main.getChildren().removeAll(formIMCcalc,formLib,formBHConverter/**,formRomain,formArmee**/)
+        //    main.getChildren().add(formRomain);
+        //});
+
+        Close.setOnAction(event -> {
+            Platform.exit();
+        });
     }
 }
