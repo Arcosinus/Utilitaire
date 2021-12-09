407bb5cc031db781e3d3d39df58baa22ce08ca47
David <david.roquain@edu.esiee-it.fr>
2021-12-09 15:16:15 +0100

    Main fonctionnel

--- src/main/java/com/example/utilitaire/controller/MainController.java
+++ src/main/java/com/example/utilitaire/controller/MainController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -20,29 +21,37 @@ public class MainController implements Initializable {
     @FXML
     private Menu menuRomain;
     @FXML
+    private Menu menuArmada;
+    @FXML
     private MenuItem Yes;
     @FXML
     private AnchorPane main;
     @FXML
+    private HBox formArmada;
+    @FXML
     private HBox formRomain;
     @FXML
-    private HBox formLibrary;
+    private VBox formLibrary;
     @FXML
     private HBox formIMCcalc;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        main.getChildren().removeAll(formIMCcalc);
-        menuIMCcalc.setOnMenuValidation(launchbinary ->{
-            main.getChildren().removeAll(formIMCcalc);
+        main.getChildren().removeAll(formArmada,formIMCcalc,formRomain,formLibrary);
+        menuArmada.setOnMenuValidation(launcharmada ->{
+            main.getChildren().removeAll(formArmada,formIMCcalc,formRomain,formLibrary);
+            main.getChildren().add(formArmada);
+        });
+        menuIMCcalc.setOnMenuValidation(launchIMC ->{
+            main.getChildren().removeAll(formArmada,formIMCcalc,formRomain,formLibrary);
             main.getChildren().add(formIMCcalc);
         });
         menuRomain.setOnMenuValidation(launchromain ->{
-            main.getChildren().removeAll(formIMCcalc,formRomain);
-            main.getChildren().add(formIMCcalc);
+            main.getChildren().removeAll(formArmada,formIMCcalc,formRomain,formLibrary);
+            main.getChildren().add(formRomain);
         });
         menuBiblio.setOnMenuValidation(launchbiblio ->{
-            main.getChildren().removeAll(formIMCcalc,formLibrary);
-            main.getChildren().add(formIMCcalc);
+            main.getChildren().removeAll(formArmada,formIMCcalc,formRomain,formLibrary);
+            main.getChildren().add(formLibrary);
         });
         Yes.setOnAction(exit ->{
             Platform.exit();
